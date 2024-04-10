51a9723784c8ffdfcea39f32d6c953ade8bb63b8
ACER\khoav <khoa93076@donga.edu.vn>
2024-04-10 17:51:57 +0700

    aaaaa

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from . import settings
 from  django.conf.urls.static import static
-from django.views.static import serve
 
 
 urlpatterns = [
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
