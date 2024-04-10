49a35fa06d7f8ee63327625390b80e6f66abe691
ACER\khoav <khoa93076@donga.edu.vn>
2024-04-10 15:33:28 +0700

    new

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECRET_KEY = 'f3dbfbb222a60dcda4fb0cd83f5484e3'
+SECRET_KEY = 'f3dbfbb222a60dcda4fb0cd83f5484e3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', "False").lower() == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS=['*']
 
 # Application definition
 
